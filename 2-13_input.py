7fb67c794b99d06fc61be713e112c52020b6a1af
LunLun <lunlunart@gmail.com>
2014-07-19 18:14:18 -0400

    Chapter 2, complete

---
+++ 2-13_input.py
@@ -0,0 +1,16 @@
+# Get the user's name, age, and income.
+name = input('What is your name? ')
+age = int(input('What is your age? '))
+income = float(input('What is your income? '))
+
+# Display the data.
+print('Here is the data you entered:')
+print('Name:', name)
+print('Age:', age)
+print('Income:', income)
+
+# Nosy program.
+
+# Question: So if you enter a integer for the name, or a string for the numbers,
+# it obviously doesn't work. There will be a way to not stop the whole program
+# if someone's inatentive, right?  
---
+++ 2-14_simple_math_input.py
@@ -0,0 +1,10 @@
+# I just wanna try something, see if I understand well.
+# Same as simple_math but combined with input
+
+salary = float(input('What is your salary?: '))
+bonus = float(input('What do you think your bonus will be?: '))
+
+pay = salary + bonus
+
+print("Then you'd be making", pay)
+print("Congrats!")
